b2081100e9c5e6b95bec3e8bb44139631a27ba77
Augustus Chong <112513956+AugustusChong@users.noreply.github.com>
2023-03-27 13:43:39 -0700

    Fixed send email php, added PHPMailer, made image sizes smaller to load faster

---
+++ src/php/send-email.php
@@ -0,0 +1,38 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+
+$mail = new PHPMailer(true);
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+try {
+    $mail -> SMTPDebug = SMTP::DEBUG_SERVER;
+    $mail -> isSMTP();
+    $mail -> Host = 'smtp.gmail.com';
+    $mail -> SMTPAuth = true;
+    $mail -> Username = 'ad.honorem.confido@gmail.com';
+    $mail -> Password = 'pwthgsfkoellmyai';
+    $mail -> SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail -> Port = 587;
+
+    $mail -> setFrom($email, $name);
+    $mail -> addAddress('augustus.chong.dev@gmail.com');
+    $mail -> addReplyTo($email, $name);
+
+    $mail -> isHTML(true);
+    $mail -> Subject = 'New Message From ' . $name . ' (' . $email . ')';
+    $mail -> Body = $message;
+
+    $mail -> send();
+    header('Location: ./sent.html');
+} catch (Exception $e) {
+    echo "Email could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+
+?>
