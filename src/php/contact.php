e8b9a94dd757b1387aecb7707f7f344027ce6a02
Augustus Chong <112513956+AugustusChong@users.noreply.github.com>
2023-03-28 11:07:58 -0700

    Fixed email contact php

--- src/php/contact.php
+++ src/php/contact.php
@@ -6,7 +6,7 @@ use PHPMailer\PHPMailer\Exception;
 require '../../vendor/autoload.php';
 include ("../../config/config.php");
 
-$developmentMode = false;
+$developmentMode = true;
 $mail = new PHPMailer(true);
 
 $name = $_POST['name'];
@@ -14,22 +14,26 @@ $email = $_POST['email'];
 $message = $_POST['message'];
 
 try {
-    // $mail -> SMTPDebug = SMTP::DEBUG_SERVER;
+    $mail -> SMTPDebug = SMTP::DEBUG_SERVER;
     $mail -> isSMTP();
-    $mail -> Host = 'smtp.gmail.com';
+    $mail -> Host = 'augustuschong.com';
     $mail -> SMTPAuth = true;
     $mail -> Username = $Username;
     $mail -> Password = $Password;
     $mail -> SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    $mail -> Port = 465;
+    $mail -> Port = 587;
 
-    $mail -> setFrom($email, $name);
+    $mail -> setFrom('contact@augustuschong.com', $name);
     $mail -> addAddress($PersonalEmail);
     $mail -> addReplyTo($email, $name);
 
-    $mail -> isHTML(true);
-    $mail -> Subject = 'New Message From ' . $name . ' (' . $email . ')';
-    $mail -> Body = $message;
+    $mail -> isHTML(false);
+    $mail -> Subject = 'New Message from ' . $name . ' (' . $email . ')';
+    $mail -> Body = <<<EOT
+    Email: {$email}
+    Name: {$name}
+    Message: {$message}
+    EOT;;
 
     $mail -> send();
     header('Location: ./sent.html');
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -129,10 +129,8 @@ window.onload = function () {
       $(this).children(".sitePreview").hide();
     });
 
-  $(document).ready(function () {
-    $("#contactForm").ajaxForm(function () {
-      alert("Thank you for your message! Message was sent successfully.");
-    });
+  $("#contactForm").ajaxForm(function () {
+    alert("Thank you for your message! Message was sent successfully.");
   });
 
   function showSkillsHeader(e) {
